5976cace5b0ecd58619fc0ff0c48f4aee9a3247e
SJCaldwell <shane.caldwell12@ncf.edu>
2018-04-20 14:46:31 -0400

    Removed stuff I don't understand, added scales

--- js/reading_level.js
+++ js/reading_level.js
@@ -1,85 +1,65 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-/* 
- * value accessor - returns the value to encode for a given data object.
- * scale - maps value to a visual display encoding, such as a pixel position.
- * map function - maps from data value to display value
- * axis - sets up axis
- */ 
-
-// setup x 
-var xValue = function(d) { return d.num_lines;}, // data -> value
-    xScale = d3.scaleLinear().range([0, width]), // value -> display
-    xMap = function(d) { return xScale(xValue(d));}, // data -> display
-    xAxis = d3.axisBottom();
-
-// setup y
-var yValue = function(d) { return d.reading_level;}, // data -> value
-    yScale = d3.scaleLinear().range([height, 0]), // value -> display
-    yMap = function(d) { return yScale(yValue(d));}, // data -> display
-    yAxis = d3.axisLeft();
-
-// setup fill color
-var cValue = function(d) { return d.group;},
-  color = d3.scaleOrdinal(d3.schemeCategory10);
-
-
-// add the graph canvas to the body of the webpage
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-// add the tooltip area to the webpage
-var tooltip = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
 
 d3.csv("../data/reading_level/shrek3_reading.csv").then(function(data) {
 
-  // don't want dots overlapping axis, so add in buffer to data domain
-xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+    var width = 600; //width of canvas
+    var height = 300; //height of canvas
+    var padding = 20;
+    
+    const color = {
+        "hero" : "green",
+        "villain": "purple",
+        "supporting": "brown"
+    };
+    
+    console.log("RUNNIN");
+    // add the graph canvas to the body of the webpage
+    var svg = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+      .append("g");
+    
+    // add the tooltip area to the webpage
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
 
-  // x-axis
-  svg.append("g")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Number of Lines");
+    //Create scale functions
+    var xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return d.num_lines; })])
+        .range([padding, width - padding * 2]);
 
-  // y-axis
-  svg.append("g")
-      .call(yAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Flesch-Kincaid Score");
+    var yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d) { return d.reading_level; })])
+        .range([height - padding, padding]);
+
+    var aScale = d3.scaleSqrt()
+        .domain([0, d3.max(data, function(d) { return d.vocab; })])
+        .range([0, 10]);
 
   // draw dots
   svg.selectAll(".dot")
       .data(data)
     .enter().append("circle")
       .attr("class", "dot")
-      .attr("r", 10)
-      .attr("cx", xMap)
-      .attr("cy", yMap)
-      .style("fill", function(d) { return color(cValue(d));}) 
+      .attr("r", function(d){
+          return aScale(d.vocab)
+      })
+      .attr("cx", function(d){
+          return xScale(d.num_lines)
+        })
+      .attr("cy", function(d){
+          return yScale(d.reading_level)
+        })
+      .style("opacity", 0.3)
+      .style("fill", function(d) { 
+          return color[d.group];
+        }) 
       .on("mouseover", function(d) {
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
-          tooltip.html(d.name + "<br/> (" + xValue(d) 
-	        + ", " + yValue(d) + ")")
+          tooltip.html(d.name + "<br/> (" + d.num_lines
+	        + ", " + d.reading_level + ")")
                .style("left", (d3.event.pageX + 5) + "px")
                .style("top", (d3.event.pageY - 28) + "px");
       })
@@ -88,26 +68,4 @@ yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
                .duration(500)
                .style("opacity", 0);
       });
-
-  // draw legend
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
-
-  // draw legend colored rectangles
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  // draw legend text
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d;})
 });
