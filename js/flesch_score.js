0d2c0a757f8628335ea7c8e378be0e699711bea0
SJCaldwell <shane.caldwell12@ncf.edu>
2018-04-23 17:32:31 -0400

    Need to update styling but it calculates the score.

--- js/flesch_score.js
+++ js/flesch_score.js
@@ -22,8 +22,27 @@ function countWords(text){
 }
 
 function countSyllables(text){
+    var re = /\S+/g;
+    var out =[];
+    var match;
+    while ((match = re.exec(text)) != null){
+        out.push(match[0])
+    }
+    syllable_count = 0
 
+    for (var i = 0; i < out.length; i++) {
+        syllable_count = syllable_count + count_syllable_for_word(out[i])
+    }
+    return syllable_count 
 }
+
+function count_syllable_for_word(word) {
+    word = word.toLowerCase();                                     //word.downcase!
+    if(word.length <= 3) { return 1; }                             //return 1 if word.length <= 3
+      word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');   //word.sub!(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '')
+      word = word.replace(/^y/, '');                                 //word.sub!(/^y/, '')
+      return word.match(/[aeiouy]{1,2}/g).length;                    //word.scan(/[aeiouy]{1,2}/).size
+  }
 
 function createCounts(text){
     var sentences = countSentences(text);
@@ -42,12 +61,13 @@ function precisionRound(number, precision) {
   }
   
 function calculateReadingLevel(counts){
+    console.log(counts)
     var SENTENCE_WEIGHT = 0.39;
     var WORD_WEIGHT = 11.8;
     var ADJUSTMENT = 15.59;
     var score_holder = document.getElementById('flesch_num')
-    var score_result = (SENTENCE_WEIGHT * (counts.word / counts.sentence)) +
-    (WORD_WEIGHT * (counts.syllable / counts.word)) -
-    ADJUSTMENT;
-    score_holder.innerHTML = precisionRound(score_result, 2);
+    var score_result = (SENTENCE_WEIGHT * (counts.word / counts.sentence)) + (WORD_WEIGHT * (counts.syllable / counts.word)) - ADJUSTMENT;
+    if (isFinite(score_result) && !isNaN(score_result)){
+        score_holder.innerHTML = precisionRound(score_result, 2);
+    }
 }
