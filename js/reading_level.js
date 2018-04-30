17270c39f298ee11f2cd8611f0a7f320a6e2787b
SJCaldwell <shane.caldwell12@ncf.edu>
2018-04-30 11:56:13 -0400

    Going to hell for making things global, probably

--- js/reading_level.js
+++ js/reading_level.js
@@ -7,7 +7,11 @@ var rowConverter = function(d) {
         vocab: parseInt(d.vocab)
     };
 }
-
+var svg = null;
+var xScale = null;
+var yScale = null;
+var aScale = null;
+var tooltip = null;
 d3.csv("../data/reading_level/shrek_trilogy_reading.csv", rowConverter, function(data) {
 
     var margin = {top: 10, right: 20, bottom: 50, left: 70},
@@ -23,25 +27,25 @@ d3.csv("../data/reading_level/shrek_trilogy_reading.csv", rowConverter, function
 
     console.log("RUNNIN");
     // add the graph canvas to the body of the webpage
-    var svg = d3.select("#read_chart").append("svg")
+    svg = d3.select("#read_chart").append("svg")
         .attr("width", width)
         .attr("height", height)
     
     // add the tooltip area to the webpage
-    var tooltip = d3.select("#read_chart").append("div")
+    tooltip = d3.select("#read_chart").append("div")
         .attr("class", "tooltip")
         .style("opacity", 0);
 
     //Create scale functions
-    var xScale = d3.scaleLinear()
+    xScale = d3.scaleLinear()
         .domain([0, d3.max(data, function(d) { return d.num_lines; })])
         .range([padding, width - padding * 2]);
 
-    var yScale = d3.scaleLinear()
+    yScale = d3.scaleLinear()
         .domain([0, d3.max(data, function(d) { return d.reading_level; })])
         .range([height - padding, padding]);
 
-    var aScale = d3.scaleSqrt()
+    aScale = d3.scaleSqrt()
         .domain([0, d3.max(data, function(d) { return d.vocab; })])
         .range([0, 10]);
 
@@ -218,7 +222,7 @@ d3.select("button#trilogy")
             .attr("class", "small")
         d3.select("button#trilogy")
             .attr("class", "small is-selected")
-        d3.csv("../data/reading_level/shrek_trilogy_reading.csv").then(function(data) {
+        d3.csv("../data/reading_level/shrek_trilogy_reading.csv", rowConverter, function(data) {
             //do redrawing here
         });
         console.log("dankdank");
@@ -230,8 +234,64 @@ d3.select("button#shrek")
             .attr("class", "small")
         d3.select("button#shrek")
             .attr("class", "small is-selected")
-    d3.csv("../data/reading_level/shrek_reading.csv", function(data) {
+    d3.csv("../data/reading_level/shrek_reading.csv", rowConverter, function(data) {
         console.log("shrek 1");
+        // draw dots
+        svg.selectAll(".dot").remove();
+
+        var margin = {top: 10, right: 20, bottom: 50, left: 70},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+    
+        //var width = 700; //width of canvas
+        //var height = 300; //height of canvas
+        var padding = 40;
+        
+        // again scaleOrdinal
+        var color = d3.scaleOrdinal(d3.schemeCategory20);
+    
+        console.log("RUNNIN");
+        // add the graph canvas to the body of the webpage
+        //Create scale functions
+        var formatAsPercentage = d3.format(".1%");
+        svg.selectAll(".dot")
+        .data(data)
+        .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", function(d){
+            return aScale(d.vocab)
+        })
+        .attr("cx", function(d){
+            return xScale(d.num_lines)
+            })
+        .attr("cy", function(d){
+            return yScale(d.reading_level)
+            })
+        .style("opacity", 0.3)
+        .style("fill", function(d) { 
+            return color(d.group);
+            }) 
+        .style('stroke-width', '2px')
+        .style('stroke', 'none')
+        .on("mouseover", function(d) {
+            d3.select(this).style('stroke', 'black')
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            
+            tooltip.html(d.name + "<br/> READING LEVEL:" + d.reading_level + "<br/>"
+                + "VOCAB: " + d.vocab + " UNIQUE WORDS"
+                + '<img style="width:100px;height:150px;" src="img/original/' + d.name.toLowerCase().split(' ').join('') + '.jpg" alt="' + d.name + '">')
+                .style("left", (d3.event.pageX + 5) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+        })
+
+        .on("mouseout", function(d) {
+            d3.select(this).style('stroke', 'none')
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
     });
 });
 
@@ -242,7 +302,7 @@ d3.select("button#shrek2")
             .attr("class", "small")
         d3.select("button#shrek2")
             .attr("class", "small is-selected")
-        d3.csv("../data/reading_level/shrek2_reading.csv").then(function(data){
+        d3.csv("../data/reading_level/shrek2_reading.csv", rowConverter, function(data){
             //do redrawing here
 
         });
@@ -255,7 +315,7 @@ d3.select("button#shrek3")
             .attr("class", "small")
         d3.select("button#shrek3")
             .attr("class", "small is-selected")
-        d3.csv("../data/reading_level/shrek2_reading.csv").then(function(data){
+        d3.csv("../data/reading_level/shrek3_reading.csv", rowConverter, function(data){
             //do redrawing here
 
         });
